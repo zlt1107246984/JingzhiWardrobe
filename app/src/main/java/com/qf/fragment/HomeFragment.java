d1ee1312f9377215c9ab88729d2e8650c975ee8d
CurryXuxiong <652337875@qq.com>
2015-11-24 09:13:29 +0800

    homefragment布局改变

--- app/src/main/java/com/qf/fragment/HomeFragment.java
+++ app/src/main/java/com/qf/fragment/HomeFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 
 import com.qf.adapter.ViewpagerAdapter;
 import com.qf.autoscrollviewpager.AutoScrollViewPager;
+import com.qf.custom.ImgNavView;
 import com.qf.jingzhiwardrobe.R;
 
 /**
@@ -18,6 +19,7 @@ public class HomeFragment extends Fragment {
 
     private AutoScrollViewPager autoScrollViewPager;
     private ViewpagerAdapter adapter;
+    private ImgNavView inv;
     private int imageIds[] = new int[]{R.mipmap.ad_1,R.mipmap.ad_2,R.mipmap.ad_3};
     @Nullable
     @Override
@@ -28,8 +30,10 @@ public class HomeFragment extends Fragment {
     }
 
     private void initView(View view) {
+
         adapter = new ViewpagerAdapter(getActivity(),imageIds);
         if (view != null){
+            inv = (ImgNavView) view.findViewById(R.id.image_nav_view);
             autoScrollViewPager = (AutoScrollViewPager) view.findViewById(R.id.auto_view_pager);
             autoScrollViewPager.setInterval(2000);//设置循环的时间间隔
             autoScrollViewPager.setSlideBorderMode(AutoScrollViewPager.SLIDE_BORDER_MODE_NONE);
